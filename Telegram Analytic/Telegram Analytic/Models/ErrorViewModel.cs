4bb9356d1d4e1e0d3d3c412036a599fe1b07569d
Ksenia <kn_ksusha@mail.ru>
2025-10-06 19:52:39 +0300

    Ptoject initialized

---
+++ Telegram Analytic/Telegram Analytic/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Telegram_Analytic.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
